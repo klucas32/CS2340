c766f3d244ae97980d55694f20345419010599e0
MOGEDI\mogedi <mahadgd94@gmail.com>
2018-09-06 14:20:50 -0400

    formatted and file has compiled successfully

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -4,103 +4,105 @@ import java.lang.Math;
 
 import java.util.ArrayList;
 
-
 /**
-
- *  A simple class for person 2
-
- *  returns their name and a
-
- *  modified string
-
+ * 
+ * A simple class for person 2
+ * 
+ * returns their name and a
+ * 
+ * modified string
  *
-
+ * 
+ * 
  * @author Bob
-
+ * 
  * @version 1.1
-
+ * 
  */
 
 public class Person2 {
 
-    /** Holds the persons real name
-     */
-
-    private String name;
-
-        /**
-         * The constructor, takes in the persons
-
-	 * name
-
-	 * @param pname the person's real name
-
+	/**
+	 * Holds the persons real name
 	 */
 
-	 public Person2(String pname) {
-
-	     name = pname;
-
-	 }
+	private String name;
 
 	/**
-
-	 * This method should take the string
-
-	 * input and return its characters in
-
-	 * random order.
-
-	 * given "gtg123b" it should return
-
-	 * something like "g3tb1g2".
-
-	 *
-
-	 * @param input the string to be modified
-
-	 * @return the modified string
-
+	 * The constructor, takes in the persons
+	 * 
+	 * name
+	 * 
+	 * @param pname the person's real name
+	 * 
 	 */
 
-	private String calc(String input) {
+	public Person2(String pname) {
 
-	  //Person 2 put your implementation here
-
-          String output ="";
-
-          while(input.length() != 0){
-
-              int randomNumber = (int)(Math.random() * (input.length() + 1));
-
-              output += input.charAt(randomNumber);
-
-              input = (input.length() <= 1) ? "" : input.substring(0, randomNumber - ((randomNumber != 0) ? 1 : 0)) + ((randomNumber != (input.length() - 1)) ? input.substring(randomNumber + 1) : ""));
-
-          }
-
-	  return output;
+		name = pname;
 
 	}
 
 	/**
-
-	 * Return a string rep of this object
-
-	 * that varies with an input string
-
+	 * 
+	 * This method should take the string
+	 * 
+	 * input and return its characters in
+	 * 
+	 * random order.
+	 * 
+	 * given "gtg123b" it should return
+	 * 
+	 * something like "g3tb1g2".
 	 *
+	 * 
+	 * 
+	 * @param input the string to be modified
+	 * 
+	 * @return the modified string
+	 * 
+	 */
 
+	private String calc(String input) {
+
+		// Person 2 put your implementation here
+
+		String output = "";
+
+		while (input.length() != 0) {
+
+			int randomNumber = (int) (Math.random() * (input.length() + 1));
+
+			output += input.charAt(randomNumber);
+
+			input = (input.length() <= 1) ? ""
+					: input.substring(0, randomNumber - ((randomNumber != 0) ? 1 : 0))
+							+ ((randomNumber != (input.length() - 1)) ? input.substring(randomNumber + 1) : "");
+
+		}
+
+		return output;
+
+	}
+
+	/**
+	 * 
+	 * Return a string rep of this object
+	 * 
+	 * that varies with an input string
+	 *
+	 * 
+	 * 
 	 * @param input the varying string
-
+	 * 
 	 * @return the string representing the
- 
+	 * 
 	 *         object
 	 */
 
 	public String toString(String input) {
 
-	  return name + calc(input);
+		return name + calc(input);
 
 	}
 
