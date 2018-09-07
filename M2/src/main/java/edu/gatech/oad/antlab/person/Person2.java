5c835baf0b019f4f8023ff5ba87ac8f83179201a
koukaakiva <42851960+koukaakiva@users.noreply.github.com>
2018-09-06 21:26:54 -0400

    Add files via upload
    Fixed code and formatting.

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,109 +1,91 @@
 package edu.gatech.oad.antlab.person;
 
-import java.lang.Math;
-
-import java.util.ArrayList;
+import java.util.Random;
 
 /**
- * 
  * A simple class for person 2
- * 
+ * <p>
  * returns their name and a
- * 
+ * <p>
  * modified string
  *
- * 
- * 
  * @author Bob
- * 
  * @version 1.1
- * 
  */
 
 public class Person2 {
 
-	/**
-	 * Holds the persons real name
-	 */
+    /**
+     * Holds the persons real name
+     */
 
-	private String name;
+    private String name;
 
-	/**
-	 * The constructor, takes in the persons
-	 * 
-	 * name
-	 * 
-	 * @param pname the person's real name
-	 * 
-	 */
+    /**
+     * The constructor, takes in the persons
+     * <p>
+     * name
+     *
+     * @param pname the person's real name
+     */
 
-	public Person2(String pname) {
+    public Person2(String pname) {
 
-		name = pname;
+        name = pname;
 
-	}
+    }
 
-	/**
-	 * 
-	 * This method should take the string
-	 * 
-	 * input and return its characters in
-	 * 
-	 * random order.
-	 * 
-	 * given "gtg123b" it should return
-	 * 
-	 * something like "g3tb1g2".
-	 *
-	 * 
-	 * 
-	 * @param input the string to be modified
-	 * 
-	 * @return the modified string
-	 * 
-	 */
+    /**
+     * This method should take the string
+     * <p>
+     * input and return its characters in
+     * <p>
+     * random order.
+     * <p>
+     * given "gtg123b" it should return
+     * <p>
+     * something like "g3tb1g2".
+     *
+     * @param input the string to be modified
+     * @return the modified string
+     */
 
-	private String calc(String input) {
+    private String calc(String input) {
+        // Person 2 put your implementation here
+        String output = "";
+        while (input.length() != 0) {
+            System.out.println("input: " + input);
+            if (input.length() <= 1) {
+                output += input;
+                input = "";
+            } else {
+                int randomNumber = new Random().nextInt(input.length() - 1);
+                System.out.println("randomnumber: " + randomNumber);
+                output += input.charAt(randomNumber);
+                if (randomNumber == 0) {
+                    input = input.substring(1);
+                } else if (randomNumber == input.length() - 1){
+                    input = input.substring(0, input.length() - 1);
+                }else{
+                    input = input.substring(0, randomNumber) + input.substring(randomNumber + 1);
+                }
+            }
+            System.out.println("output: " + output);
+        }
+        return output;
+    }
 
-		// Person 2 put your implementation here
-
-		String output = "";
-
-		while (input.length() != 0) {
-
-			int randomNumber = (int) (Math.random() * (input.length() + 1));
-
-			output += input.charAt(randomNumber);
-
-			input = (input.length() <= 1) ? ""
-					: input.substring(0, randomNumber - ((randomNumber != 0) ? 1 : 0))
-							+ ((randomNumber != (input.length() - 1)) ? input.substring(randomNumber + 1) : "");
-
-		}
-
-		return output;
-
-	}
-
-	/**
-	 * 
-	 * Return a string rep of this object
-	 * 
-	 * that varies with an input string
-	 *
-	 * 
-	 * 
-	 * @param input the varying string
-	 * 
-	 * @return the string representing the
-	 * 
-	 *         object
-	 */
-
-	public String toString(String input) {
-
-		return name + calc(input);
-
-	}
-
+    /**
+     * Return a string rep of this object
+     * <p>
+     * that varies with an input string
+     *
+     * @param input the varying string
+     * @return the string representing the
+     * <p>
+     * object
+     */
+    public String toString(String input) {
+        return name + calc(input);
+    }
 }
--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -1,48 +1,52 @@
 package edu.gatech.oad.antlab.person;
+
 /**
- *  A simple class for person 3
- *  returns their name and a
- *  reversed string 
- *  
- *  @author  Bob
- *  @version 1.1
+ * A simple class for person 3
+ * returns their name and a
+ * reversed string
+ *
+ * @author Bob
+ * @version 1.1
  */
 public class Person3 {
-    /** Holds the persons real name */
-	private String name;
-	
-	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
-	 */
-	public Person3(String pname){
-	  name = pname;
-	}
-	
-	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
-	 *
-	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
-	 */
-	public String toString(String input) {
-	  return name + calc(input);
-	}
-	
-	/**
-	 * This method should take the string
-	 * input and return its reverse.
-	 * given "gtg123b" it should return
-	 * b321gtg.
-	 *
-	 * @param input the string to be reversed
-	 * @return the reversed string
-	 */
-	private String calc(String input) {
-	    String[] tempArray = new String[input.length()];
+    /**
+     * Holds the persons real name
+     */
+    private String name;
+
+    /**
+     * The constructor, takes in the persons
+     * name
+     *
+     * @param pname the person's real name
+     */
+    public Person3(String pname) {
+        name = pname;
+    }
+
+    /**
+     * Return a string rep of this object
+     * that varies with an input string
+     *
+     * @param input the varying string
+     * @return the string representing the
+     * object
+     */
+    public String toString(String input) {
+        return name + calc(input);
+    }
+
+    /**
+     * This method should take the string
+     * input and return its reverse.
+     * given "gtg123b" it should return
+     * b321gtg.
+     *
+     * @param input the string to be reversed
+     * @return the reversed string
+     */
+    private String calc(String input) {
+	    /*String[] tempArray = new String[input.length()];
 	    String finalString = "";
 	    for (int i = 0; i < input.length(); i++) {
 	        String tempString = input.substring(i, i + 1);
@@ -51,6 +55,11 @@ public class Person3 {
 	    for (int j = tempArray.length - 1; j >= 0; j++) {
 	        finalString += tempArray[j];
         }
-	    return finalString;
-	}
+	    return finalString;*/
+	    String output = "";
+	    for(int i = input.length() - 1; i >= 0; i--){
+	        output += input.charAt(i);
+        }
+        return output;
+    }
 }
